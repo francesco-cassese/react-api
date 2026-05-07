b7a37b75a6272ff2efc1a141d79e1b942224b525
Francesco Cassese <francescocassese9@gmail.com>
2026-05-07 15:19:26 +0200

    feat: eseguita chiamata api  e stampata in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+const urlAttrici = 'https://lanciweb.github.io/demo/api/actresses/';
 
 function App() {
+
+  const chiamataApi = url => {
+    return (
+      fetch(url)
+        .then(response => {
+          return response.json();
+        })
+        .then(json => {
+          console.log(json);
+        })
+    )
+  }
+
+  useEffect(() => {
+    console.log('Eseguito');
+    chiamataApi(urlAttrici);
+  }, []);
+
   return (
     <h1>Hello</h1>
   );
